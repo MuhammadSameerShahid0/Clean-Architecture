7bc5fb6ed16a8a0b63e0ea08b443c32ade6227e9
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-06-27 22:18:51 +0500

    Domain

---
+++ BuberDiner.Domain/Common/Errors/Errors.Authentication.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+
+namespace BuberDiner.Domain.Common.Errors;
+public static partial class Errors
+{
+	public static class Authentication
+	{
+		public static Error InvalidCredentials = Error.Validation(
+			code: "Auth.InvalidCredentials",
+			description: "Invalid Credentials");
+	}
+}
